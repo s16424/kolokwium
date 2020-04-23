ec60b7b3a24cea9e403631fe0ad1418cea001245
unknown <s16424@pjwstk.edu.pl>
2020-04-23 09:52:34 +0200

    zadanie 2

--- kolokwium_s16424_AP/kolokwium_s16424_AP/Controllers/PrescriptionController.cs
+++ kolokwium_s16424_AP/kolokwium_s16424_AP/Controllers/PrescriptionController.cs
@@ -1,4 +1,5 @@
-﻿using kolokwium_s16424_AP.Services;
+﻿using kolokwium_s16424_AP.DTOs;
+using kolokwium_s16424_AP.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -41,15 +42,71 @@ namespace kolokwium_s16424_AP.Controllers
                 {
                     return BadRequest("Taki lekarz nie istnieje");
                 }
-                else
-                    return Ok(service.GetPresciptions2(nazwisko).OrderByDescending(s => s.Date));
-
+            }
+            return Ok(service.GetPresciptions2(nazwisko).OrderByDescending(s => s.Date));
             }
 
+    [HttpPost]
+    public IActionResult AddPrescription([FromBody]AddPrescriptionRequest request)
+        {
+            var pr = new AddPrescriptionRequest();
+            pr.Date = request.Date;
+            pr.DueDate = request.DueDate;
 
+            if (pr.DueDate > pr.Date)
+            {
+                return BadRequest("Zle dane - data waznosci dalsza niz data wystawienia recepty");
+            }
 
+            pr.idDoctor = request.idDoctor;
+            pr.idPatient = request.idPatient;
 
+            using (var con = new SqlConnection("Data Source=db-mssql ;Initial Catalog=s16424; Integrated Security = True"))
+            using (var com = new SqlCommand())
+            {
+                com.Connection = con;
+
+                con.Open();
+                var tran = con.BeginTransaction();
+                try
+                {
+                    com.CommandText = "SELECT * from doctor where idDoctor = @id";
+                    com.Parameters.AddWithValue("id",pr.idDoctor);
+                    var dr = com.ExecuteReader();
+
+                    if (!dr.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("taki lekarz nie istnieje");
+                    }
+                    com.CommandText = "SELECT * from patient where idPatient = @id";
+                    com.Parameters.AddWithValue("id", pr.idPatient);
+                    dr = com.ExecuteReader();
+
+                    if (!dr.Read())
+                    {
+                        tran.Rollback();
+                        return BadRequest("taki pacjent nie istnieje");
+                    }
+                        com.CommandText = "INSERT INTO Prescription VALUES @data, @datawaznosci, @idpacjenta, @iddoktora";
+                        com.Parameters.AddWithValue("data", pr.Date);
+                        com.Parameters.AddWithValue("datawaznosci", pr.DueDate);
+                        com.Parameters.AddWithValue("idpacjenta", pr.idPatient);
+                        com.Parameters.AddWithValue("iddoktora", pr.idDoctor);
+
+                    com.ExecuteNonQuery();
+                        tran.Commit();
+                    }
+                    
+                catch (SqlException ex)
+                {
+                    tran.Rollback();
+                }
+            }
+            
+            return Ok();
+        }
 
         }
     }
-}
+
---
+++ kolokwium_s16424_AP/kolokwium_s16424_AP/DTOs/AddPrescriptionRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace kolokwium_s16424_AP.DTOs
+{
+    public class AddPrescriptionRequest
+    {
+        [Required(ErrorMessage = "Podaj date wystawienia")]
+        public DateTime Date { get; set; }
+       
+        [Required(ErrorMessage = "Podaj date waznosci")]
+        public DateTime DueDate { get; set; }
+       
+        [Required(ErrorMessage = "Podaj id pacjenta")]
+        public int idPatient { get; set; }
+
+        [Required(ErrorMessage = "Podaj id lekarza")]
+        public string idDoctor { get; set; }
+    }
+}
